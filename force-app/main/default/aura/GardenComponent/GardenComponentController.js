9230d213be4374610399f5ca83fa19b005649587
Donny Buckman <monchkrit@gmail.com>
2021-03-16 13:51:15 -0700

    Moving slowly forward

--- force-app/main/default/aura/GardenComponent/GardenComponentController.js
+++ force-app/main/default/aura/GardenComponent/GardenComponentController.js
@@ -1,5 +1,20 @@
 ({
-    getSoilSurveys : function(component, event, helper) {
-        
+    doInit : function(cmp, event, helper) {
+        CompositionEvent.set("v.tableColumns", [
+            {label: 'Col1', fieldName: 'Col1', type: 'text'},
+            {label: 'Col2', fieldName: 'Col2', type: 'text'}
+            // etc etc
+        ])
+    },
+    getSoilSurveys : function(cmp) {
+        let action = cmp.get("c.makeCallout");
+        action.setCallback(this, function(response){
+            let state = response.getState();
+            if (state === "SUCCESS") {
+                cmp.set("v.soil", response.getReturnValue());
+            }
+        });
+
+        $A.enqueueAction(action);
     }
 })

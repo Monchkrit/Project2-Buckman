8d48591398b24f93990d3b787882b607313f15e1
Donny Buckman <monchkrit@gmail.com>
2021-03-16 14:57:36 -0700

    try to split the object out of the API data

--- force-app/main/default/aura/SoilComponent/SoilComponentController.js
+++ force-app/main/default/aura/SoilComponent/SoilComponentController.js
@@ -1,5 +1,8 @@
 ({
-    myAction : function(component, event, helper) {
-
+    tabinit : function(component, event, helper) {
+        component.set('v.columns', [
+            {label: 'AWC - Representative Value', fieldName: 'awc_r__c', type: 'number'},
+            {label: 'AWS025WTA - Available Water Storage', fieldName: 'om_r__c', type: 'number'}
+        ]);
     }
 })

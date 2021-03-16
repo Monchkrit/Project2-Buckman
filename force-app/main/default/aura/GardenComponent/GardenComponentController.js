9ab7b7a5450838b4c45f3fd68695e2360cb3a8e2
Donny Buckman <monchkrit@gmail.com>
2021-03-16 14:33:47 -0700

    data if moving, now I'm going to try to show it.

--- force-app/main/default/aura/GardenComponent/GardenComponentController.js
+++ force-app/main/default/aura/GardenComponent/GardenComponentController.js
@@ -1,17 +1,10 @@
 ({
-    doInit : function(cmp, event, helper) {
-        CompositionEvent.set("v.tableColumns", [
-            {label: 'Col1', fieldName: 'Col1', type: 'text'},
-            {label: 'Col2', fieldName: 'Col2', type: 'text'}
-            // etc etc
-        ])
-    },
     getSoilSurveys : function(cmp) {
         let action = cmp.get("c.makeCallout");
         action.setCallback(this, function(response){
             let state = response.getState();
             if (state === "SUCCESS") {
-                cmp.set("v.soil", response.getReturnValue());
+                cmp.set("v.soilSurveys", response.getReturnValue());
             }
         });
 
